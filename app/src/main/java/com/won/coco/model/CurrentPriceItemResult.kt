55273a000c8e40ab566bd2c20784226d83db7f33
kjw2000e <kjw2000e@naver.com>
2024-08-26 23:16:36 +0900

    fix data class 수정
    - DATA클래스 가공 "name"을 추가하여 새로운
    result data class 생성

---
+++ app/src/main/java/com/won/coco/model/CurrentPriceItemResult.kt
@@ -0,0 +1,6 @@
+package com.won.coco.model
+
+data class CurrentPriceItemResult(
+    val coinName: String,
+    val currentPriceItem: CurrentPriceItem
+)
--- app/src/main/java/com/won/coco/view/SelectViewModel.kt
+++ app/src/main/java/com/won/coco/view/SelectViewModel.kt
@@ -2,18 +2,36 @@ package com.won.coco.view
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.won.coco.model.CurrentPriceItem
+import com.won.coco.model.CurrentPriceItemResult
 import com.won.coco.repository.NetworkRepository
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class SelectViewModel() : ViewModel() {
 
+    private lateinit var resultList: ArrayList<CurrentPriceItemResult>
     val networkRepository = NetworkRepository()
 
     fun getAllCoinList() = viewModelScope.launch {
         val result = networkRepository.getCurrentCoinList()
+        resultList = ArrayList()
 
-        Timber.d(result.toString())
+        for (coin in result.data) {
+
+            try {
+                val gson = Gson()
+                val gsonToJson = gson.toJson(result.data.get(coin.key))
+                val gsonFromJson = gson.fromJson(gsonToJson, CurrentPriceItem::class.java)
+                val currentPriceItemResult = CurrentPriceItemResult(coin.key, gsonFromJson)
+
+                Timber.d(currentPriceItemResult.toString())
+                resultList.add(currentPriceItemResult)
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
     }
 }
 
